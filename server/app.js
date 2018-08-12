874fdfecf67633e48878525f0ab99f5744b7453f
Carlos Eduardo Monti <slaytanic@gmail.com>
2018-08-12 12:29:15 -0300

    Manage forwarded traffic

--- server/app.js
+++ server/app.js
@@ -71,7 +71,7 @@ app.use('/graphql', (req, res, next) => graphqlHTTP({
 })(req, res, next));
 
 function redirectToHttps(req, res, next) {
-  if (req.secure) {
+  if (req.secure || req.get('x-forwarded-proto') === 'https') {
     return next();
   }
   return res.redirect(`https://${req.headers.host}${req.url}`);
