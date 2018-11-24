21335d36abae79f2f95bf4a8208a5c57ffdb7d6b
Carlos Eduardo Monti <carlos.monti@gmail.com>
2018-11-24 15:12:52 -0300

    Switch eslint rules

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,49 +1,49 @@
-const path = require('path');
+// const path = require('path');
 
 module.exports = {
-  parser: 'babel-eslint',
-  env: {
-    es6: true,
-    node: true,
-    browser: true,
-  },
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  extends: ['airbnb'],
-  settings: {
-    'import/resolver': {
-      node: {
-        paths: path.resolve(__dirname, 'src'),
-      },
-    },
-  },
-  rules: {
-    // Allow .js files to use JSX syntax
-    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
-    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+  // parser: 'babel-eslint',
+  // env: {
+  //   es6: true,
+  //   node: true,
+  //   browser: true,
+  // },
+  // parserOptions: {
+  //   ecmaVersion: 6,
+  //   sourceType: 'module',
+  //   ecmaFeatures: {
+  //     jsx: true,
+  //   },
+  // },
+  extends: ['react-app'],
+  // settings: {
+  //   'import/resolver': {
+  //     node: {
+  //       paths: path.resolve(__dirname, 'src'),
+  //     },
+  //   },
+  // },
+  // rules: {
+  //   // Allow .js files to use JSX syntax
+  //   // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
+  //   'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
 
-    // Functional and class components are equivalent from React’s point of view
-    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prefer-stateless-function.md
-    'react/prefer-stateless-function': 'off',
+  //   // Functional and class components are equivalent from React’s point of view
+  //   // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prefer-stateless-function.md
+  //   'react/prefer-stateless-function': 'off',
 
-    // Recommend not to leave any console.log in your code
-    // Use console.error, console.warn and console.info instead
-    // https://eslint.org/docs/rules/no-console
-    'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
+  //   // Recommend not to leave any console.log in your code
+  //   // Use console.error, console.warn and console.info instead
+  //   // https://eslint.org/docs/rules/no-console
+  //   'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
 
-    'react/forbid-prop-types': 'off',
+  //   'react/forbid-prop-types': 'off',
 
-    'no-underscore-dangle': ['error', { allow: ['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] }],
-  },
-  // plugins: ["react"],
-  // extends: [
-  //   "eslint:recommended",
-  //   "plugin:react/recommended",
-  //   "plugin:prettier/recommended"
-  // ]
+  //   'no-underscore-dangle': ['error', { allow: ['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] }],
+  // },
+  // // plugins: ["react"],
+  // // extends: [
+  // //   "eslint:recommended",
+  // //   "plugin:react/recommended",
+  // //   "plugin:prettier/recommended"
+  // // ]
 };
