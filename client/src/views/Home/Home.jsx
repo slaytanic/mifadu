b7601893cf25a890ccdb2275ba9a511d757f706d
Carlos Eduardo Monti <carlos.monti@gmail.com>
2018-11-28 19:19:00 -0300

    oops

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -52,8 +52,11 @@ class Home extends Component {
               </CardHeader>
               <CardFooter stats>
                 <div className={classes.stats}>
-                  {/* <Link to="/students">Ver miembros del taller</Link> */}
-                  <Link to="/works">Ver miembros del taller</Link>
+                  {isTutor ? (
+                    <Link to="/works">Ver miembros del taller</Link>
+                  ) : (
+                    <Link to="/students">Ver miembros del taller</Link>
+                  )}
                 </div>
               </CardFooter>
             </Card>
